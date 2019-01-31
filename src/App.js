99b4633fc9d01725415cf6e47fc3defc8b913683
yaroslav <yaroslav@localhost.localdomain>
2019-01-31 02:37:50 +0200

    Eslint

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ class App extends Component {
     this.state = { currentPage: 'Schedule' }
   }
   render() {
-    return (<div>
-        <button onClick={()=>this.setState({currentPage:'Schedule'})}>Schedule</button>
-        <button onClick={() => this.setState({ currentPage: 'Teachers' })}>Teachers</button>
-        <button onClick={() => this.setState({ currentPage: 'Exams' })}>Exams</button>
+    return (
+      <div>
+        <button onClick={() => this.setState({currentPage: 'Schedule'})}>Schedule</button>
+        <button onClick={() => this.setState({ currentPage: 'Teachers'})}>Teachers</button>
+        <button onClick={() => this.setState({ currentPage: 'Exams'})}>Exams</button>
         
-        {this.state.currentPage==='Schedule' ? <Schedule /> : null}
+        {this.state.currentPage === 'Schedule' ? <Schedule /> : null}
         {this.state.currentPage === 'Teachers' ? <Teachers /> : null}
         {this.state.currentPage === 'Exams' ? <Exams /> : null}
       </div>
--- src/components/lessons components/Schedule.js
+++ src/components/lessons components/Schedule.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import Form from'./Form';
+import Chart from'./Chart';
 
-// eslint-disable-next-line react/require-render-return
-class Schedule extends Component {
+class Schedule extends Component {  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: undefined
+    }
+  }
 
-    gettingSchedule = async (event) => {
-        event.preventDefault();
-            //const group = event.target.elements.group.value;
-            // eslint-disable-next-line
-            const group = event.target.elements.Schedule.value;
-            //const api_url = await fetch(`https://api.rozklad.hub.kpi.ua/groups.json/?search=${group}`);
-            const api_url = await fetch(`http://api.rozklad.org.ua/v2/groups/${group}/timetable`);
-            const data = await api_url.json();
-            //console.log(data)
-        }
-       
-    render(){
-        return(
-            <div>
-               <Form gettingSchedule={this.gettingSchedule}/>
-            </div>
-        )
-    };
+  gettingSchedule = async (event) => {
+    event.preventDefault();
+
+    const group = event.target.elements.Schedule.value;
+    //const api_url = await fetch(`https://api.rozklad.hub.kpi.ua/groups.json/?search=${group}`);
+    const api_url = await fetch(`http://api.rozklad.org.ua/v2/groups/${group}/timetable`);
+    const data = await api_url.json();
+    this.setState({
+      data: data.data.weeks
+    });
+  }
+      
+  render(){
+    return(
+      <div>
+        <Form gettingSchedule={this.gettingSchedule}/>
+        <Chart weeks={this.state.data}/> 
+      </div>
+    )
+  };
 }
+
 export default Schedule;
