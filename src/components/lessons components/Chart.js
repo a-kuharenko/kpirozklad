608ac75cc63f9ace3dd7813d39683ab89381eac3
yaroslav <yaroslav@localhost.localdomain>
2019-02-03 21:31:06 +0200

    Output information using a single function

--- src/components/lessons components/Chart.js
+++ src/components/lessons components/Chart.js
@@ -2,19 +2,50 @@ import React, {Component} from 'react';
 
 class Chart extends Component {
   render(props) {
-    let template = null;
-    if (this.props.data){
-      const weeks = this.props.data.weeks;
-      template = Object.keys(weeks).map(item => weeks[item]);
-    } else {
-      template = this.props.error
-    };
-    const templateString = JSON.stringify(template);
 
+    let chart = [];
+
+    if (this.props.data){
+      for(let w = 1; w <= 2; w++){
+
+        if(w === 1){
+          chart.push(<h2>Перший тиждень</h2>);
+        } else {
+          chart.push(<h2>Другий тиждень</h2>);
+        }
+
+        for(let d = 1; d <= 6; d++){
+
+          const days = this.props.data.weeks[w].days[d];
+           let dayName = 
+            <ul><b>{days.day_name}</b></ul>
+            chart.push(dayName);
+
+          for(let l = 0; l <= this.props.data.weeks[w].days[d].lessons.length; l++){
+            try{
+              const lessons = days.lessons[l];
+
+              let lessonsInfo = 
+              <div>
+                  <u>{lessons.lesson_number} пара: <small> {lessons.time_start}-{lessons.time_end}</small></u>
+                  <div>{lessons.lesson_name}</div>
+              </div>;
+              chart.push(lessonsInfo);
+            } catch {
+              console.log('error');
+            }
+            
+          }
+        }
+      }
+    } else {
+       chart.push(this.props.error);
+    };
+    
     return(
-      <ul>
-        {templateString}
-      </ul>
+      <div>
+        <div>{chart}</div>
+      </div>
     )
   }
 };
