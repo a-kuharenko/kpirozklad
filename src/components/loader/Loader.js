3d2d527ad6a94c30cc9d4bfce143b5ce56ff359d
yaroslav <yaroslav@localhost.localdomain>
2019-02-21 18:42:33 +0200

    Added loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loader.css';
+
+export default () => (
+  <div className="loader">
+    <div className="l_main">
+      <div className="l_square"><span></span><span></span><span></span></div>
+      <div className="l_square"><span></span><span></span><span></span></div>
+      <div className="l_square"><span></span><span></span><span></span></div>
+      <div className="l_square"><span></span><span></span><span></span></div>
+    </div>
+  </div>
+  
+);
+
